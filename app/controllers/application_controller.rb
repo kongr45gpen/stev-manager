3c5ac420ab042a533feeeb87df2932f3299c5b14
kongr45gpen <electrovesta@gmail.com>
2017-04-18 13:56:20 +0300

    Make the interface friendlier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,20 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def after_sign_in_path_for(resource_or_scope)
-    root_path
+    events_path
   end
 
   def set_locale
     I18n.locale = current_user.try(:locale) || I18n.default_locale
+  end
+end
+
+class ActiveAdmin::Devise::SessionsController
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || events_path
+  end
+
+  def after_sign_out_path_for(resource)
+    '/'
   end
 end
--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,5 +1,5 @@
 class ScheduleController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   include ScheduleHelper
 
   def index
