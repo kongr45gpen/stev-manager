029731f851a0c03ea042f953d32eadf6181e9cf1
kongr45gpen <electrovesta@gmail.com>
2017-04-25 00:25:32 +0300

    Improve administration dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,19 +3,12 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
-
     columns do
       column do
         panel "Recent Logins" do
           ul do
             User.order(last_sign_in_at: "ASC").where.not(last_sign_in_at: nil).limit(5).map do |user|
-              li link_to(user.email, admin_user_path(user)) + ' ' + time_ago_in_words(user.last_sign_in_at) + ' ago'
+              li link_to(user.email, admin_user_path(user)) + ' ' + time_ago_in_words(user.current_sign_in_at) + ' ' + I18n.t(:ago)
             end
           end
         end
