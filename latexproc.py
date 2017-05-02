8bdce70150a67cf1b0c6dd994936539a280b6d9a
kongr45gpen <electrovesta@gmail.com>
2017-05-02 21:54:45 +0300

    Don't show rule for the last entry

--- latexproc.py
+++ latexproc.py
@@ -5,18 +5,26 @@ output = open('schedule.ruh.aux','w')
 input  = open('schedule.rul.aux','r')
 
 oldline = None
+objects = []
 for row in input:
   event, line = row.split()
 
   broken = (line != oldline) and (oldline != None)
 
-  formatted_json = json.dumps({'event':event, 'line':line,'broken':broken})
-  colorful_json = highlight(unicode(formatted_json, 'UTF-8'), lexers.JsonLexer(), formatters.TerminalFormatter())
-  print colorful_json,
+  object = {'event':event, 'line':line,'broken':broken}
+  objects.append(object)
 
   oldline = line
 
-  if broken:
+if objects:
+  objects[-1]['broken'] = False
+
+for object in objects:
+  formatted_json = json.dumps(object)
+  colorful_json = highlight(unicode(formatted_json, 'UTF-8'), lexers.JsonLexer(), formatters.TerminalFormatter())
+  print colorful_json,
+
+  if object['broken']:
     output.write(event + "\n")
 
 input.close()
