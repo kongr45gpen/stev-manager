7f77f2f8d265b8fcdb4f5e3b9d6c689f506919ac
kongr45gpen <electrovesta@gmail.com>
2018-03-13 14:55:41 +0200

    Rework event imports for 2018

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -25,36 +25,38 @@ class ImportController < ApplicationController
   def parse_events(data)
     Encoding.default_external = Encoding::UTF_8
 
+    # Map column_name -> column_id
+    co = Hash.new
+    data.third.each_index { |k| co[data.third[k]] = k }
+
     data.select {|x| x.first.to_s.is_i?}.each do |datum|
       @submitter = Submitter.new(
-        surname: datum[7].to_s.force_encoding("utf-8"),
-        name: datum[8].to_s.force_encoding("utf-8"),
-        property: datum[9].to_s.force_encoding("utf-8"),
-        faculty: datum[10].to_s.force_encoding("utf-8"),
-        school: datum[11].to_s.force_encoding("utf-8"),
-        phone: datum[12].to_s.force_encoding("utf-8"),
-        email: datum[13].to_s.force_encoding("utf-8")
+        surname: datum[co["epitheto"]].to_s.force_encoding("utf-8"),
+        name: datum[co["onoma"]].to_s.force_encoding("utf-8"),
+        property: datum[co["idiotita"]].to_s.force_encoding("utf-8"),
+        faculty: datum[co["sholi"]].to_s.force_encoding("utf-8"),
+        school: datum[co["tmima"]].to_s.force_encoding("utf-8"),
+        phone: datum[co["tilefono_stathero_kinito"]].to_s.force_encoding("utf-8"),
+        email: datum[co["e_mail"]].to_s.force_encoding("utf-8")
       )
 
       @event = Event.new(
-        team: datum[14].to_s.force_encoding("utf-8"),
-        title: datum[15].to_s.force_encoding("utf-8"),
-        other: datum[21].to_s.force_encoding("utf-8"),
-        proposed_space: datum[22].to_s.force_encoding("utf-8"),
-        proposed_time: datum[23].to_s.force_encoding("utf-8")
+        team: datum[co["onoma_foititikis_omadas"]].to_s.force_encoding("utf-8"),
+        title: datum[co["titlos_drastiriotitas"]].to_s.force_encoding("utf-8"),
+        other: datum[co["an_epilexate_allo3"]].to_s.force_encoding("utf-8"),
+        proposed_space: datum[co["horos_diexagogis"]].to_s.force_encoding("utf-8"),
+        proposed_time: datum[co["proteinomenes_imerominies"]].to_s.force_encoding("utf-8")
       )
 
       @event.submitter = @submitter
       kinds = []
-      kinds << "theatre" if datum[16]
-      kinds << "music" if datum[17]
-      kinds << "photography" if datum[18]
-      kinds << "sports" if datum[19]
-      kinds << "other" if datum[20]
+      kinds << "theatre" if not datum[co["Θέατρο"]].blank?
+      kinds << "music" if not datum[co["Μουσική"]].blank?
+      kinds << "photography" if not datum[co["Φωτογραφία"]].blank?
+      kinds << "sports" if not datum[co["Αθλητισμός"]].blank?
+      kinds << "other" if not datum[co["Άλλο"]].blank?
       @event.kind = kinds.first
       @event.fields = kinds
-
-      # return @submitter
 
       @submitter.save
       @event.save
